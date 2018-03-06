d5de7265d20ec9359eac3b5b5592b568be3f768f
Simona Cotin <simona.cotin@gmail.com>
2018-03-06 16:13:08 +0000

    add link to vscodecandothat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: `Visual Studio Code Can Do That?`,
+    subtitle: `All the best things about Visual Studio Code that nobody ever bothered to tell you`,
+    url: 'http://vscodecandothat.com/'
+  });
 });
 
 module.exports = router;
